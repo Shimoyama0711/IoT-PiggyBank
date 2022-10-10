aba88e101eca069cc71c93b3b92abc9d875778e6
Shimoyama Ayumu <m19284@g.metro-cit.ac.jp>
2022-10-10 23:40:34 +0900

    RGBテスト用

--- public/js/test.js
+++ public/js/test.js
@@ -1,6 +1,10 @@
 $(function () {
-    setInterval(updateDate, 250);
+    updateDate();
+    getRGB();
 
+    setInterval(updateDate, 100);
+
+    $("#update-rgb").on("click", getRGB);
     $("#getLocation").on("click", getLocation);
     $("#roll-dice").on("click", rollDice);
 });
@@ -13,6 +17,22 @@ function updateDate() {
 
     $("#date").text(date.getFullYear() + "/" + (date.getMonth() + 1).toString().padStart(2, '0') + "/" + date.getDate().toString().padStart(2, '0') + " (" + weekdays[date.getDay()] + ")");
     $("#time").text(date.getHours().toString().padStart(2, '0') + ":" + date.getMinutes().toString().padStart(2, '0') + ":" + date.getSeconds().toString().padStart(2, '0'));
+}
+
+// RGBを更新する
+function getRGB() {
+    $.ajax({
+        type: "GET",
+        url: "/rgb"
+    }).done(function (data) {
+        const json = JSON.parse(JSON.stringify(data));
+        const red   = json["red"].toString(16).toUpperCase().padStart(2, '0');
+        const green = json["green"].toString(16).toUpperCase().padStart(2, '0');
+        const blue  = json["blue"].toString(16).toUpperCase().padStart(2, '0');
+        const colorCode = "#" + red + green + blue;
+
+        $("#palette").css("background", colorCode);
+    });
 }
 
 // 位置情報を取得
