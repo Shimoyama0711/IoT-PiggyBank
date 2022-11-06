a105791cf83e95aa1f954aa825c166b90506deeb
Shimoyama Ayumu <m19284@g.metro-cit.ac.jp>
2022-11-07 08:25:13 +0900

    MySQLライブラリのアップグレード

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-import {serve} from "https://deno.land/std@0.158.0/http/server.ts";
-import {serveDir} from "https://deno.land/std@0.158.0/http/file_server.ts";
-import {Client} from "https://deno.land/x/mysql@v2.10.2/mod.ts";
+import {serve} from "https://deno.land/std@0.161.0/http/server.ts";
+import {serveDir} from "https://deno.land/std@0.161.0/http/file_server.ts";
+import {Client} from "https://deno.land/x/mysql@v2.10.3/mod.ts";
 
 const client = await new Client().connect({
     hostname: "localhost",
