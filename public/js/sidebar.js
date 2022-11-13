0b204af72e25e167cf166dea9ab67a208fa22707
Shimoyama Ayumu <m19284@g.metro-cit.ac.jp>
2022-11-13 17:09:55 +0900

    sidebar更新

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -2,6 +2,7 @@ $(function () {
     const pathname = location.pathname;
     const pages = ["account", "dashboard", "settings", "deposit", "danger"];
 
+    // Sidebar関連
     for (let i = 0; i < pages.length; i++) {
         const n = pages[i];
 
@@ -16,11 +17,32 @@ $(function () {
 
     const cookie = document.cookie.split(";");
 
+    // ユーザーがサインインしていた場合
     for (let i = 0; i < cookie.length; i++) {
         const key = cookie[i].split("=")[0];
         const value = cookie[i].split("=")[1];
 
-        if (key === "name")
-            $("#user-name").text(value);
+        if (key === "name") {
+            if (value.length > 0) {
+                getUserInfo(value).done(function (response) {
+                    const avatar = response["avatar"] ?? "default.jpg";
+
+                    $("#user-name").text(value);
+                    $("#avatar").attr("src", avatar);
+                });
+            } else {
+                $("#user-name").text("Not Signed In");
+                $("#avatar").attr("src", "default.jpg");
+            }
+        }
     }
 });
+
+function getUserInfo(name) {
+    return $.ajax({
+        url: "/get-user-info",
+        type: "POST",
+        dataType: "json",
+        data: JSON.stringify({name: name})
+    });
+}
